02cb09abc1cfafc46f1c65f37741faddd5a1c8ad
Achintya <achintya21110010@snuchennai.edu.in>
2023-01-12 22:40:49 +0530

    Generating and printing matrix done with recursion

--- CIA-1.py
+++ CIA-1.py
@@ -1,55 +1,46 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import random
 
+random.seed(69)
 match = 5
 mismatch = -4
-char_set = "ACGT"
-char_count_1 = [0, 0, 0, 0]
-char_count_2 = [0, 0, 0, 0]
-        
-s1 = ""
-s2 = ""
 
-while len(s1) < 16:
-    s = random.choice(char_set)
-    if (s1.count(s) == 4):
-        continue
+def generate_string(length, count):
+    if length == 0:
+        return ""
     else:
-        s1 += s
-        
+        letters = ['A'] * count['A'] + ['C'] * count['C'] + ['G'] * count['G'] + ['T'] * count['T']
+        return random.choice(letters) + generate_string(length - 1, count)
 
-while len(s2) < 16:
-    s = random.choice(char_set)
-    if (s2.count(s) == 4):
-        continue
+count = {'A' : 4, 'C' : 4, 'G' : 4, 'T' : 4}
+
+genome_1 = generate_string(16, count)
+genome_2 = generate_string(16, count)
+
+alignment_matrix = [[0 for x in range(17)] for y in range(17)]
+
+
+def print_matrix(matrix, i, j, c, flag=0):
+    if flag == 0:
+        print('    ', end = '')
+        for x in genome_1:
+            print(x, end = ' ')
+        print()
+        print('  ', end='')
+        flag = 1
+    if i == len(matrix):
+        return
+    if j == len(matrix[i]):
+        print()
+        if c != 16:
+            print(genome_2[c], end = ' ')
+        print_matrix(matrix, i+1, 0, c+1, 1)
     else:
-        s2 += s
+        print(matrix[i][j], end = ' ')
+        print_matrix(matrix, i, j+1, c, 1)
         
 
-alignment = [[0 for i in range(len(s1))] for j in range(len(s2))]
+print(genome_1)
+print(genome_2)
+print()
+print_matrix(alignment_matrix, 0, 0, 0)
 
-def score(alignment, s1, s2, row):
-    if (row == len(s1)):
-        return alignment
-    
-    for j in range(1, len(s2)+1):
-        if s1[row-1] == s2[j-1]:
-            alignment[row][j] = alignment[row-1][j-1] + match
-        else:
-            num = max(alignment[row-1][j], alignment[row-1][j-1], alignment[row][j-1])
-            num += mismatch
-            if (num < 0):
-                num = 0
-            alignment[row][j] = num
-            
-    score(alignment, s1, s2, row+1)
-    
-
-alignment = score(alignment, s1, s2, 1)
-print(alignment)
